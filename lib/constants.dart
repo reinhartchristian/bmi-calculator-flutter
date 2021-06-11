18d2bde544d9b5ef54adffe87c9fb7f0890b88e5
Reinhart Christian <reinhartchristian17@gmail.com>
2021-06-11 11:57:52 +0700

    half done

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kActiveCardColour = Color(0XFF1D1E33);
+const kInactiveCardColour = Color(0XFF111328);
+const kBottomContainerHeight = 80.0;
+const kBottomContainerColour = Color(0XFFEB2555);
+const kContentStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0XFF8D8E98),
+);
+const kNumberStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
